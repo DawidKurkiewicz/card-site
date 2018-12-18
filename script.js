21bde9761ab0bf794700dc9c3ee3f5326b8b09fd
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-18 20:23:01 +0100

    displaying weather

--- script.js
+++ script.js
@@ -21,12 +21,35 @@ $(document).ready(function () {
 });
 const key = '0b3d75e5a49f2a267f054a0a60bed6f3'
 
-setInterval(function () {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&APPID=${key}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-}, 30000);
+function createNode(element) {
+    return document.createElement(element);
+}
 
+function append(parent, el) {
+  return parent.appendChild(el);
+}
 
-
+const ul = document.getElementById('weather');
+const url = `http://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&APPID=${key}`;
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+    console.log(data)
+  let currentWeather = data.weather;
+  return currentWeather.map(function() {
+    let li = createNode('li'),
+        img = createNode('img'),
+        span = createNode('span');
+    span.innerHTML =
+    `tempretaure : ${data.main.temp}
+    pressure: ${data.main.pressure}
+    ${data.clouds.all}`;
+    append(li, img);
+    append(li, span);
+    append(ul, li);
+  })
+})
+.catch(function(error) {
+  console.log(error);
+});
 
