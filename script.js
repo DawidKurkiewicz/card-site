227b1da42f3d357d0fc9be850b99115cd01d45f8
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-19 12:09:27 +0100

    changed whole body elements

--- script.js
+++ script.js
@@ -19,37 +19,44 @@ $(document).ready(function () {
         $("#myCarousel").carousel(0);
     });
 });
-const key = '0b3d75e5a49f2a267f054a0a60bed6f3'
 
 function createNode(element) {
     return document.createElement(element);
 }
 
 function append(parent, el) {
-  return parent.appendChild(el);
+    return parent.appendChild(el);
 }
 
+
+const key = '0b3d75e5a49f2a267f054a0a60bed6f3'
 const ul = document.getElementById('weather');
 const url = `http://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&APPID=${key}`;
 fetch(url)
-.then((resp) => resp.json())
-.then(function(data) {
-    console.log(data)
-  let currentWeather = data.weather;
-  return currentWeather.map(function() {
-    let li = createNode('li'),
-        img = createNode('img'),
-        span = createNode('span');
-    span.innerHTML =
-    `tempretaure : ${data.main.temp}
-    pressure: ${data.main.pressure}
-    ${data.clouds.all}`;
-    append(li, img);
-    append(li, span);
-    append(ul, li);
-  })
-})
-.catch(function(error) {
-  console.log(error);
-});
+    .then((resp) => resp.json())
+    .then(function (data) {
+
+        console.log(data)
+        let currentWeather = data.weather;
+        return currentWeather.map(function () {
+            let li = createNode('li'),
+                div = createNode('div')
+            div.innerHTML =
+                `<div class=temperature>temperatura : ${data.main.temp}</div>
+                <div> zachmurzenie: ${data.clouds.all} % </div>
+                <div> wilgotność: ${data.main.humidity} %</div>`;
+
+            append(li, div);
+            append(ul, li);
+
+
+        })
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+
+
+
+
 
