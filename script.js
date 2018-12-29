85d6790daa423119b2049ccf453e43e423f24d02
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-29 13:56:26 +0100

    changed all weather structure, to fetch and refresh easier

--- script.js
+++ script.js
@@ -20,43 +20,45 @@ $(document).ready(function () {
     });
 });
 
-function createNode(element) {
+createNode = (element) => {
     return document.createElement(element);
 }
 
-function append(parent, el) {
+append = (parent, el) => {
     return parent.appendChild(el);
 }
 
+createStructure = () => {
+    return `<div class="temp"></div>
+            <div class="humidity"></div>
+            <div>Aktualne warunki pogodowe:<img class="icon" src=""></div>`;
+}
 
-const key = '0b3d75e5a49f2a267f054a0a60bed6f3'
-const ul = document.getElementById('weather');
-const url = `http://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&APPID=${key}`;
-fetch(url)
+fetch_data = (url) => {
+  fetch(url)
     .then((resp) => resp.json())
     .then(function (data) {
-
-        console.log(data)
         let currentWeather = data.weather;
-        return currentWeather.map(function () {
-            let li = createNode('li'),
-                div = createNode('div')
-            div.innerHTML =
-                `<div class=temperature>temperatura : ${data.main.temp} C</div>
-                <div> wilgotność: ${data.main.humidity} %</div>
-                <div> zachmurzenie: <img src = 'http://openweathermap.org/img/w/${data.weather[0].icon}.png'></div>`;
-
-            append(li, div);
-            append(ul, li);
-
-
+        return currentWeather.map(function() {
+            document.querySelector('.temp').innerHTML = `temperatura: ${data.main.temp} C`;
+            document.querySelector('.humidity').innerHTML = `wilgotność: ${data.main.humidity} %`;
+            document.querySelector(".icon").src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
         })
     })
     .catch(function (error) {
         console.log(error);
     })
+}
 
+const weather_div = document.getElementById('weather');
+const key = '0b3d75e5a49f2a267f054a0a60bed6f3'
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&APPID=${key}`;
 
+weather_div.innerHTML = createStructure();
+fetch_data(url);
 
+setInterval(() => {
+  fetch_data(url);
+}, 30000);
 
 
